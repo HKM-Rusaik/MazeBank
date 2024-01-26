1e12d2bc2ee5557f65ac3c6baa0d5f3514b471b4
HKM-Rusaik <hkmrusaik@gmail.com>
2024-01-26 11:35:22 +0530

    Client Button click action full details

--- src/main/java/com/example/mazebank/Controllers/LoginController.java
+++ src/main/java/com/example/mazebank/Controllers/LoginController.java
@@ -1,14 +1,27 @@
 package com.example.mazebank.Controllers;
 
+import com.example.mazebank.Models.Model;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class LoginController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
     public ChoiceBox acc_selector;
     public Label payee_address_lbl;
     public TextField payee_address_fld;
     public Label password_lbl;
     public TextField password_fld;
     public Label error_lbl;
+    public Button login_btn;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        login_btn.setOnAction(actionEvent -> Model.getInstance().getViewFactory().showClientWindow());
+    }
 }
